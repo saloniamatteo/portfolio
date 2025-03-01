575114d3f01ea68b9ebd37b12ba82ce81acbcb5d
saloniamatteo <saloniamatteo@pm.me>
2025-03-01 19:55:54 +0100

    Cirrus: upgrade to version 0.8 & switch to Sass

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
-    // Disable polyfill
     build: {
+        // Disable polyfill
         modulePreload: {
             polyfill: false,
         },
+
+        // Rollup options
         rollupOptions: {
             // Tree-shaking
             treeshake: {
@@ -18,25 +20,40 @@ export default defineConfig({
     },
     // Disable comments in JS
     esbuild: { legalComments: 'none' },
+
+    // Plugins
     plugins: [
+        // Laravel
         laravel({
             input: [
-                'resources/css/cirrus.min.css',
+                'resources/css/cirrus.css',
                 'resources/css/fonts/fonts.css',
                 'resources/css/overrides.css',
                 'resources/js/main.js',
             ],
             refresh: true,
         }),
+
         // PurgeCSS
         purge({
             paths: [
                 'resources/views/**',
             ],
-            output: 'resources/css/cirrus.min.css',
+            output: 'resources/css/cirrus.css',
+            // This fixes escaped prefixes (sm:, lg:, ...)
+            extractors: [
+                {
+                    extractor: (content) => {
+                        return content.match(/[A-z0-9-:\/]+/g) || []
+                    },
+                    extensions: ['php', 'html']
+                }
+            ],
             fontFace: true,
             keyframes: true,
-            safelist: ["active"],
+            safelist: [
+                "active",           // Header
+            ],
         }),
     ],
 });
